f65ac562b2ff9734cf2a39ad7e201cd796eaf72b
freshaluck <ikenyedaniel@gmail.com>
2020-04-02 11:06:53 -0400

    new update

--- asset/src/reducers/errorReducer.js
+++ asset/src/reducers/errorReducer.js
@@ -7,7 +7,7 @@ const initialState = {
 }
 
 export default function (state = initialState, action) {
-  switch (action.payload) {
+  switch (action.type) {
     case GET_ERRORS:
       return {
         msg: action.payload.msg,
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('config');
+const morgan = require('morgan');
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -15,9 +16,10 @@ const login = require('./routes/login');
 // }
 
 app.use(cors({ origin: true, credentials: true }))
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cookieParser());
+// app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost/assets')
   .then(() => console.log('Connected to mongoDB'))
